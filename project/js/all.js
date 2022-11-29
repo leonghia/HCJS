37eac349b6103a835f60b8f1cfd6ada054a4f899
leonghia <leonghiacnn@gmail.com>
2022-11-29 21:18:11 +0700

    adding hover menu for template

--- project/js/all.js
+++ project/js/all.js
@@ -1,1 +1,15 @@
-// This is the script file for all pages
+// Showing/hiding sub menu on hover
+const showMenu = event => {
+    let menu = event.target;
+    let sub = menu.lastElementChild;
+    setTimeout(() => sub.style.display = 'block', 300);
+}
+
+const hideMenu = event => {
+    let menu = event.target;
+    let sub = menu.lastElementChild;
+    setTimeout(() => sub.style.display = 'none', 300);
+}
+
+
+
