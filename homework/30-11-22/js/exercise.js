338336f95ed1c6dbdec4ceddf749c5211d106155
leonghia <leonghiacnn@gmail.com>
2022-12-09 11:09:58 +0700

    edit exercise

--- homework/30-11-22/js/exercise.js
+++ homework/30-11-22/js/exercise.js
@@ -1,78 +1,88 @@
 // Get JSON from web API
-const currentWeather = () => {
-    let para = document.querySelector('input').value;
+const currentWeather = (city) => {
     let xhttp = new XMLHttpRequest();
-
-xhttp.onload = function () {
-    // Transform JSON into object/array
-    let result = this.responseText;
-    let data = JSON.parse(result);
-    
-    // Working with the object/array
-    console.log(data);
-    const container = document.querySelector('.container');
-    const myRow = document.querySelector('.row');
-
-    myRow.innerHTML = "";
-
-    for (let i = 0; i < data.list.length; i++) {
-        // Add article element with class col-3
-        const myArticle = document.createElement('article');
-        myRow.appendChild(myArticle);
-        myArticle.classList.add('col-12', 'col-xl-3');
-
-        // Add div element with class card
-        const myDiv1 = document.createElement('div');
-        myArticle.appendChild(myDiv1);
-        myDiv1.classList.add('card', 'text-bg-secondary', 'mb-3');
-        myDiv1.style.setProperty('background-color', 'RGBA(108,117,125,var(--bs-bg-opacity,.6))', 'important');
-
-        // Add div element with class card body
-        const myDiv2 = document.createElement('div');
-        myDiv1.appendChild(myDiv2);
-        myDiv2.classList.add('card-body');
-
-        // Add h6 element into card body
-        const myH6 = document.createElement('h6');
-        myDiv2.appendChild(myH6);
+    xhttp.onload = function() {
+        // Transform JSON into object/array
+        let result = this.responseText;
+        let data = JSON.parse(result);
         
-        // Add time content into myH6
-        const time = data.list[i].dt_txt;
-        myH6.innerText = time;
+        // Working with the object/array
+        console.log(data);
+        const container = document.querySelector('.container');
+        const myRow = document.querySelector('.row');
 
-        // Add div with display flex to hold temperature and icon image
-        const myDiv3 = document.createElement('div');
-        myDiv2.appendChild(myDiv3);
-        myDiv3.style.display = 'flex';
-        myDiv3.style.alignItems = 'center';
-        myDiv3.style.justifyContent = 'space-between';
-        myDiv3.style.height = '60px';
+        // Clear the page
+        myRow.innerHTML = "";
 
-        // Add h2 element with temperature value
-        const myH2 = document.createElement('h2');
-        myDiv3.appendChild(myH2);
-        myH2.innerText = data.list[i].main.temp;
-        myH2.innerHTML += '<sup>o</sup>C';
+        // Generate new page
+        for (let i = 0; i < data.list.length; i++) {
+            // Add article element with class col-3
+            const myArticle = document.createElement('article');
+            myRow.appendChild(myArticle);
+            myArticle.classList.add('col-12', 'col-xl-3');
 
-        // Add icon image
-        const myImage = document.createElement('img');
-        myDiv3.appendChild(myImage);
-        myImage.src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
+            // Add div element with class card
+            const myDiv1 = document.createElement('div');
+            myArticle.appendChild(myDiv1);
+            myDiv1.classList.add('card', 'text-bg-secondary', 'mb-3');
+            myDiv1.style.setProperty('background-color', 'RGBA(108,117,125,var(--bs-bg-opacity,.6))', 'important');
 
-        // Add p element with description
-        const myP = document.createElement('p');
-        myDiv2.appendChild(myP);
-        myP.innerText = data.list[i].weather[0].description;
+            // Add div element with class card body
+            const myDiv2 = document.createElement('div');
+            myDiv1.appendChild(myDiv2);
+            myDiv2.classList.add('card-body');
+
+            // Add h6 element into card body
+            const myH6 = document.createElement('h6');
+            myDiv2.appendChild(myH6);
+            
+            // Add time content into myH6
+            const time = data.list[i].dt_txt;
+            myH6.innerText = time;
+
+            // Add div with display flex to hold temperature and icon image
+            const myDiv3 = document.createElement('div');
+            myDiv2.appendChild(myDiv3);
+            myDiv3.style.display = 'flex';
+            myDiv3.style.alignItems = 'center';
+            myDiv3.style.justifyContent = 'space-between';
+            myDiv3.style.height = '60px';
+
+            // Add h2 element with temperature value
+            const myH2 = document.createElement('h2');
+            myDiv3.appendChild(myH2);
+            myH2.innerText = data.list[i].main.temp;
+            myH2.innerHTML += '<sup>o</sup>C';
+
+            // Add icon image
+            const myImage = document.createElement('img');
+            myDiv3.appendChild(myImage);
+            myImage.src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
+
+            // Add p element with description
+            const myP = document.createElement('p');
+            myDiv2.appendChild(myP);
+            myP.innerText = data.list[i].weather[0].description;
+        }
     }
-}
-
-xhttp.open("GET", `https://api.openweathermap.org/data/2.5/forecast?q=${para},vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric`);
+xhttp.open("GET", `https://api.openweathermap.org/data/2.5/forecast?q=${city},vietnam&appid=09a71427c59d38d6a34f89b47d75975c&units=metric`);
 
 xhttp.send();
-
 }
 
-currentWeather();
+currentWeather('London');
+
+// Change city based on event-listener
+const changeCity = (event) => {
+    let city = event.target.value;
+    currentWeather(city);
+}
+
+// Add event-listener to all buttons
+let myButtons = document.getElementsByClassName('btn');
+for (let i = 0; i < myButtons.length; i++) {
+    myButtons[i].addEventListener('click', changeCity);
+}
 
 // Other stylings
 document.querySelector('div').style.paddingTop = '50px';
